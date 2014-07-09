a80cbbc65ae7f7b35a5ccbd81ce4f99154347b51
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-07-09 17:52:21 -0500

    src

--- lib/Alien/LibMagic.pm
+++ lib/Alien/LibMagic.pm
@@ -3,4 +3,29 @@ package Alien::LibMagic;
 use strict;
 use warnings;
 
+use parent 'Alien::Base';
+
+sub Inline {
+	return unless $_[-1] eq 'C'; # Inline's error message is good
+	my $self = __PACKAGE__->new;
+	+{
+		LIBS => $self->libs,
+		INC => $self->cflags,
+		AUTO_INCLUDE => '#include "magic.h"',
+	};
+}
+
 1;
+
+__END__
+# ABSTRACT: Alien package for the libmagic library
+
+=pod
+
+=head1 Inline support
+
+This module supports L<Inline's with functionality|Inline/"Playing 'with' Others">.
+
+=head1 SEE ALSO
+
+L<file(1)>, L<file command|http://darwinsys.com/file/>, L<File::LibMagic>
