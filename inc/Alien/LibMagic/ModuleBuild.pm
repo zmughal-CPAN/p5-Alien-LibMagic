ae841bb55509e4ffb39e367cd485ac7456d9f863
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-07-27 22:32:45 -0500

    ModuleBuild for MSWin32 configure script

--- inc/Alien/LibMagic/ModuleBuild.pm
+++ inc/Alien/LibMagic/ModuleBuild.pm
@@ -4,16 +4,24 @@ use strict;
 use warnings;
 
 use parent 'Alien::Base::ModuleBuild';
-use if $^O eq 'MSWin32', 'Alien::MSYS' => qw(msys);
 use Archive::Extract;
 use File::Copy;
 use File::Spec;
+use Module::Load;
 
 sub alien_do_commands {
 	my ($self, $phase) = @_;
 	my $dir = $self->config_data( 'working_directory' );
 
 	if( $^O eq 'MSWin32' ) {
+		load 'Alien::MSYS';
+		$self->alien_build_commands([
+			map {
+				my $c = $_;
+				$c =~ s/^%pconfigure/sh %pconfigure/;
+				$c
+			} @{$self->alien_build_commands}
+		]);
 		my $extract_to = $phase eq 'build'
 			? $dir
 			: $self->alien_library_destination;
@@ -35,9 +43,9 @@ sub alien_do_commands {
 
 		}
 
-		msys {
+		Alien::MSYS::msys( sub  {
 			$self->SUPER::alien_do_commands($phase);
-		};
+		} );
 	} else {
 		$self->SUPER::alien_do_commands($phase);
 	}
