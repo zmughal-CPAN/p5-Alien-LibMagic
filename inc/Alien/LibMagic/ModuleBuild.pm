e1826563dcf0e35b643041784f0409ab8faff753
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-07-30 01:46:31 -0500

    add win32 build notes

--- inc/Alien/LibMagic/ModuleBuild.pm
+++ inc/Alien/LibMagic/ModuleBuild.pm
@@ -8,13 +8,29 @@ use Archive::Extract;
 use File::Copy;
 use File::Spec;
 use Module::Load;
-
+# NOTE
+# win32 build process
+# extract inc\mingw-libgnurx-2.5.1-src.tar.gz
+# patch --binary < inc\patch\mingw-libgnurx-2.5.1
+# cd mingw-libgnurx-2.5.1; sh .\configure; make
+# cd file-5*\
+#set CPPFLAGS=-I$abs_dir/_alien/mingw-libgnurx-2.5.1
+#set LDFLAGS=-L$abs_dir/_alien/mingw-libgnurx-2.5.1
+#set PATH=%PATH%;$abs_dir/_alien/mingw-libgnurx-2.5.1
+#sh .\configure
+#make
 sub alien_do_commands {
 	my ($self, $phase) = @_;
 	my $dir = $self->config_data( 'working_directory' );
 
 	if( $^O eq 'MSWin32' ) {
 		load 'Alien::MSYS';
+		my $lib = File::Spec->catfile( $dir, 'lib');
+		my $include = File::Spec->catfile($dir, 'include');
+		$ENV{PATH} = "$ENV{PATH};$lib";
+		$ENV{CPPFLAGS} = "$ENV{CPPFLAGS} -I$include";
+		use DDP; p $ENV{PATH};
+		use DDP; p $ENV{CPPFLAGS};
 		$self->alien_build_commands([
 			map {
 				my $c = $_;
@@ -26,7 +42,7 @@ sub alien_do_commands {
 			? $dir
 			: $self->alien_library_destination;
 
-		my @files = glob File::Spec->catfile( $dir, '..', 'inc', 'mingw-libgnurx*.tar.gz' );
+		my @files = glob File::Spec->catfile( $dir, '..', '..', 'inc', 'mingw-libgnurx*.tar.gz' );
 		for my $archive_file (@files) {
 			my $e = Archive::Extract->new( archive => $archive_file );
 			$e->extract( to => $extract_to );
